16e3f1a94cc6cf44c434c8afc7198540b460f7b4
RandalphvanVelden <randalph77@gmail.com>
2019-01-29 13:58:15 +0100

    got my website assesment.test working

---
+++ laravel/assesment/resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>vierdaagse</title>
+
+    Klaar voor volgende assesment
+    </head>
+    <body>
+        test
+    </body>
+</html>
